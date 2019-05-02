c8f9b620210737c620b568bbadce095cacd9b80c
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-05-02 20:34:28 +0000

    Deploy AJ-7885/webchannel to github.com/AJ-7885/webchannel.git:gh-pages

---
+++ precache-manifest.1391c043432987a0a9b030dc218fb117.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ad28b8f593cbc3d043f94a70dfc89f7d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7bd65faec068af1cc701",
+    "url": "/static/css/main.ab9a025f.chunk.css"
+  },
+  {
+    "revision": "45124f853ba07aa965c0",
+    "url": "/static/js/2.6efc73d3.chunk.js"
+  },
+  {
+    "revision": "7bd65faec068af1cc701",
+    "url": "/static/js/main.9d6af7ee.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "4839d490575102f4fd91232a0050930f",
+    "url": "/static/media/logo.4839d490.png"
+  }
+]);
